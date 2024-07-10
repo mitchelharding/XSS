b6dc05efbac0ea2e86a8f03104c8779576348f29
mitchelharding <hardingmitchel@gmail.com>
2024-07-10 09:53:28 -0500

    changed variable name

--- xss.js
+++ xss.js
@@ -1,2 +1,2 @@
-var x = new Image();
-x.src = 'https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906';
+var img = new Image();
+img.src = 'https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906';
